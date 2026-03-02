c63c89e4fe7fd6a08a3a310a2cf965bc13b1dfe6
Kompot24 <efim.popov.2017@bk.ru>
2026-03-03 00:00:08 +0800

    second commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "asd daaffaf a ff a f fad 123 ssdfdfgfgg";
+            int count = 0;
+            for (int i = 0; i < text.Length-1; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (text[i] == text[i+1])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Кол-во одинаковых букв: {count}");
         }
     }
 }
